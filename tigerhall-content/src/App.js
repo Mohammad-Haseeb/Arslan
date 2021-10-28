5f96d2dabdf411fe2224fe663d024b649e61828a
Muhammad Haseeb Hussain <64950545+Mohammad-Haseeb@users.noreply.github.com>
2021-10-29 00:58:20 +0500

    done

--- tigerhall-content/src/App.js
+++ tigerhall-content/src/App.js
@@ -1,118 +1,119 @@
 
-import {useState,useRef} from 'react';
-import { Input ,} from "@chakra-ui/react"
+import { useState, useRef } from 'react';
+import { Input, } from "@chakra-ui/react"
 import logo from './animation/load.gif'
 import './App.css';
-import {useQuery } from "@apollo/client";
-import { getAllMessages  } from "./gqlSchema/index.js"
+import { useQuery } from "@apollo/client";
+import { getAllMessages } from "./gqlSchema/index.js"
 
-import {BlogPostWithImage} from './Component/cardInfo'
+import { BlogPostWithImage } from './Component/cardInfo'
 
-import {Context} from './GloabalState/index';
+import { Context } from './GloabalState/index';
 
 function App() {
-  let { data,  loading} =  useQuery(getAllMessages)
-    
-       let loadingState= useState(false);
-   let searchState= useState("")
-   const inputEl = useRef("");
+  let { data, loading } = useQuery(getAllMessages)
+
+  let loadingState = useState(false);
+  let searchState = useState("")
+  const inputEl = useRef("");
 
 
- 
-  console.log("Checker",data)
+
+  console.log("Checker", data)
   console.log("Check")
 
   const handleChange = (event) => {
-     searchState[1](inputEl.current.value)
-     loadingState[1](false);
+    searchState[1](inputEl.current.value)
+    loadingState[1](false);
 
   }
-    if(loading) {
-      return (
-        <>
-         <Loader/>
-        </>
-      )
-    }
-    if(data){
-      return (
-        
-        <>
-        <Context.Provider  value={searchState}>
-        
-        <div className="Main-Search">
-        <div className="App">
-          <div>
-        <p className="searchHeading" style={{color:"white"}}>Search : </p>
-        <Input
-          bg="#003238"
-          ref={inputEl}
-  border="white"
-          value={inputEl.current.value}
-          onChange={handleChange}
-          placeholder="Type Any Keyword"
-          size="sm"
-        />
-        </div>
-         </div>
-        </div>
-        
+  if (loading) {
+    return (
+      <>
+        <Loader />
+      </>
+    )
+  }
+  if (data) {
+    return (
+
+      <>
+        <Context.Provider value={searchState}>
+
+          <div className="Main-Search">
+            <div className="App">
+              <div>
+                <p className="searchHeading" style={{ color: "white" }}>Search : </p>
+                <Input
+                  bg="#003238"
+                  ref={inputEl}
+                  border="white"
+                  value={inputEl.current.value}
+                  onChange={handleChange}
+                  placeholder="Type Any Keyword"
+                  size="md"
+                  w="40"
+                />
+              </div>
+            </div>
+          </div>
+
           <div className="mainCard-Container">
 
-         { 
-            
-            data.contentCards.edges.map((obj, index) => {
+            {
 
-                    if(searchState[0]==="" ){
-                      return(
-                  
-                        <div key={index}>{<BlogPostWithImage data={obj}/>}</div>
-                        
-                      )
-                    }
-                    else if (obj.name.toLowerCase().includes(searchState[0].toLocaleLowerCase())){
-                      setTimeout(()=>{ 
-                             loadingState[1](true);
-                      }, 3000)
-                       if ( loadingState[0]){
-                      return (
+              data.contentCards.edges.map((obj, index) => {
 
-                        <div key={index}>{<BlogPostWithImage data={obj}/>}</div>
+                if (searchState[0] === "") {
+                  return (
 
-                      )
+                    <div key={index}>{<BlogPostWithImage data={obj} />}</div>
 
-                       }else{
-                             return(
-                               <>
-                               <Loader />
-                               </>
-                             )
-                       }  
-                    }
-                    
-               
-          })
-          }
-         </div> 
-         
-         </Context.Provider>
-        </>
-      );
-     
-    }
+                  )
+                }
+                else if (obj.name.toLowerCase().includes(searchState[0].toLocaleLowerCase())) {
+                  setTimeout(() => {
+                    loadingState[1](true);
+                  }, 3000)
+                  if (loadingState[0]) {
+                    return (
+
+                      <div key={index}>{<BlogPostWithImage data={obj} />}</div>
+
+                    )
+
+                  } else {
+                    return (
+                      <>
+                        <Loader />
+                      </>
+                    )
+                  }
+
+                }
+
+
+              })
+            }
+          </div>
+
+        </Context.Provider>
+      </>
+    );
+
+  }
 
 
 }
 
 
-const Loader=()=>{
+const Loader = () => {
   return (
     <div >
-    <div  className="loader">
-      <div><p style={{color:'white',}}>loading</p></div>
-      <img src={logo} alt="loader"/>
+      <div className="loader">
+        <img src={logo} alt="loader" />
+      </div>
     </div>
-  </div>
   );
 }
 
--- tigerhall-content/src/Component/cardInfo.jsx
+++ tigerhall-content/src/Component/cardInfo.jsx
@@ -1,49 +1,7 @@
-//----------
-//Components
-//----------
 import React from 'react';
 import './card.css';
-
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Card = (props) => {
-//   const { firstName, lastName, title, company } = props.data.experts[0];
-//   console.log("name : ", props.data.name)
-
-
-
-//   return (
-//     <div className="card">
-//       <div className="image-box">
-//         <img src={props.img} />
-//       </div>
-//       <div className="card-body">
-//         {/* <h3>{props.c}</h3> */}
-//         {/* <h2>{props.data.name}</h2> */}
-//         <h2>{`${firstName} ${lastName}`}</h2>
-//         <h2>{title}</h2>
-//         <p>{company}</p>
-//       </div>
-//     </div>
-//   )
-
-// }
 
 export const BlogPostWithImage = (props) => {
    const {firstName, lastName,title,company} = props.data.experts[0];
@@ -52,12 +10,12 @@ export const BlogPostWithImage = (props) => {
   return (
 
     <>
-  
-  <Card style={{ width: '23rem' }}>
+   <div className="mainCOntainer">
+  <Card >
   <div >
   <Card.Img variant="top" style={{ width: '100%'  , height: '200px' , objectFit:"fill"}} src={props.data.image.uri} />
   </div>
-  <Card.Body style={{ padding:"10"}}>
+  <Card.Body className="cardContentBody">
     <p  className="category">{props.data.categories[0].name}</p>
     <p className="name">{props.data.name}</p>
     <p className="userName">
@@ -79,19 +37,10 @@ export const BlogPostWithImage = (props) => {
 </Card>
 
 
-      {/* <div className='cards'>
-        <Card
-          img={props.data.image.uri}
-          title='What I learned from my visit to The Upside Down'
-          author='Nancy Wheeler'
-          data={props.data} />
-      </div> */}
-
+     
+</div>
     </>
   )
 
 }
 
-  //----------
-  //Final render
-  //----------
